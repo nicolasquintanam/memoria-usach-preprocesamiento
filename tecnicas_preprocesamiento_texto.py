1036bfa0a86687bb6ad5c570140e89668d73afc7
Nicolas Quintana <nicolas.quintana@usach.cl>
2021-01-19 02:50:04 -0300

    se añade la técnica part of speech tagging de spacy

--- tecnicas_preprocesamiento_texto.py
+++ tecnicas_preprocesamiento_texto.py
@@ -1,6 +1,7 @@
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk import SnowballStemmer
+import es_core_news_sm
 
 
 # Técnica 'Tokenization' del Preprocesamiento de texto.  En simples palabras, transforma el 
@@ -25,3 +26,14 @@ def remove_stopwords(word_list):
 def stemming(word_list):
     spanishstemmer = SnowballStemmer('spanish')
     return [spanishstemmer.stem(token) for token in word_list]
+
+# Técnica 'Etiquetado Gramatical'. Permite asignar a cada palabra del listado, una etiqueta mor-
+# fosintáctica, es decir, su categoría gramatical.  Si la palabra es un verbo, adverbio, sustan-
+# tivo, etc.
+def pos_tagging(word_list):
+    nlp = es_core_news_sm.load()
+    doc = nlp(' '.join(word_list))
+    new_word_list = []
+    for word in doc:
+        new_word_list.append((word.text, word.pos_))
+    return new_word_list
