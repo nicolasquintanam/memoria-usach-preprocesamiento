16184b01b02fdc22dd8db4038ed9e77f7e7ed149
Nicolas Quintana <nicolas.quintana@usach.cl>
2021-02-15 19:25:29 -0300

    prueba fe3

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ print("creando listado de palabras con frecuencia 1")
 words_frequency_1 = crear_listado_palabras_frecuencia_1(input)
 print("Ya se creó el listado")
 
-print("-- Comenzó preprocesamiento --")
-one_hot_paralelize(input, False, flujo_experimental_2, words_frequency_1, 'flujo_exp_2')
+print("-- Comenzó preprocesamiento con flujo_experimental_3 --")
+one_hot_paralelize(input, True, flujo_experimental_3, words_frequency_1, 'flujo_exp_3')
 print("-- Finalizó preprocesamiento --")
