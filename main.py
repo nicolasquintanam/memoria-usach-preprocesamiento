9cd629577652c555f401a83d5cf78ab88bb1e17d
Nicolas Quintana <nicolas.quintana@usach.cl>
2021-02-16 19:55:53 -0300

    prueba con flujo base

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ print("creando listado de palabras con frecuencia 1")
 words_frequency_1 = crear_listado_palabras_frecuencia_1(input)
 print("Ya se creó el listado")
 
-print("-- Comenzó preprocesamiento con flujo_experimental_10 --")
-one_hot_paralelize(input, True, flujo_experimental_10, words_frequency_1, 'flujo_exp_10')
+print("-- Comenzó preprocesamiento con flujo_base --")
+one_hot_paralelize(input, False, flujo_base, words_frequency_1, 'flujo_base')
 print("-- Finalizó preprocesamiento --")
