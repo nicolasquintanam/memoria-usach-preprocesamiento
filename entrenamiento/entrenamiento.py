5b576a2065796972f012af115504ea931a144665
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-02-19 21:47:18 -0300

    prueba entrenamiento

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -1,10 +1,14 @@
 from time import time
 import csv
 import multiprocessing
+import sklearn
 
 
 def datasetFromFile(lineaCSV):
-    return lineaCSV.replace('\n', '').split(';')
+    listado = lineaCSV.replace('\n', '').split(';')
+    map_object = map(int, listado)
+    list_of_integers = list(map_object)
+    return list_of_integers
 
 input_dataset = 'flujo_base_dataset.csv'
 input_category = 'flujo_base_tenders-category.txt'
@@ -28,24 +32,16 @@ print('dataset')
 print(elapsed_time)
 
 
-'''
-for i in range(len(lineas_dataset)):
-    linea_category = lineas_category[i].replace('\n', '')
-    categorias.append(linea_category)
-    print(linea_category)
 
-with open(input_dataset, newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
-
-#print(data)
 
 ########## PREPARAR LA DATA ##########
 #Importamos los datos de la misma librería de scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 algoritmo = GaussianNB()
-X_train, X_test, y_train, y_test = train_test_split(data, categorias, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(resultado, categories, test_size=0.2)
 algoritmo.fit(X_train, y_train)
 print('entrenado')
-'''
+
+predictions = algoritmo.predict(X_test)
+print(predictions)
