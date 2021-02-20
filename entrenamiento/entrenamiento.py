1a6a170cf40da1710c941fb57fcd0264c5dd1e33
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-02-19 23:17:46 -0300

    entrenamiento

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -5,13 +5,16 @@ import sklearn
 
 
 def datasetFromFile(lineaCSV):
-    listado = lineaCSV.replace('\n', '').split(';')
-    map_object = map(int, listado)
-    list_of_integers = list(map_object)
+    try:
+        listado = lineaCSV.replace('\n', '').split(';')
+        map_object = map(int, listado)
+        list_of_integers = list(map_object)
+    except:
+        print(listado)
     return list_of_integers
 
-input_dataset = 'flujo_base_dataset.csv'
-input_category = 'flujo_base_tenders-category.txt'
+input_dataset = 'flujo_experimental_1_dataset.csv'
+input_category = 'flujo_experimental_1_tenders-category.txt'
 
 start_time_category = time()
 categories = []
@@ -38,6 +41,7 @@ print(elapsed_time)
 #Importamos los datos de la misma librería de scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+print('')
 algoritmo = GaussianNB()
 X_train, X_test, y_train, y_test = train_test_split(resultado, categories, test_size=0.2)
 algoritmo.fit(X_train, y_train)
