bf8daed3bf92f4e51d4e69fd1c2ee61629890f42
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-02-23 22:04:07 -0300

    pruebas

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-filename_train_dataset = 'flujo_experimental_3_dataset_train.csv'
-filename_train_categories = 'flujo_experimental_3_tenders-category-train.txt'
+filename_train_dataset = 'flujo_base_dataset_train.csv'
+filename_train_categories = 'flujo_base_tenders-category-train.txt'
 
 algoritmo = GaussianNB()
 
@@ -14,8 +14,9 @@ file_train_dataset = open(filename_train_dataset, 'r')
 file_train_categories = open(filename_train_categories, 'r')
 lines = file_train_dataset.readlines()
 lines_category = file_train_categories.readlines()
-for i in range(100):
-    line = lines[i].replace('\n', '')
+for i in range(32529):
+    print(i)
+    line = lines[i].replace('\n', '').split(';')
     map_object = map(int, line)
     X = list(map_object)
     a = int(lines_category[i].replace('\n', ''))
