e815dc3d50032ae9ea772ec2e6d3b7c9583c8594
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-02-24 03:23:52 +0100

    correccion

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -5,18 +5,37 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-filename_train_dataset = 'flujo_base_dataset_train.csv'
-filename_train_categories = 'flujo_base_tenders-category-train.txt'
+filename_train_dataset = 'flujo_experimental_1_dataset_train.csv'
+filename_train_categories = 'flujo_experimental_1_tenders-category-train.txt'
 
 algoritmo = GaussianNB()
 
 file_train_dataset = open(filename_train_dataset, 'r')
 file_train_categories = open(filename_train_categories, 'r')
+lineass = file_train_categories.readlines()
+i = 0
+for element in file_train_dataset:
+    element = element.replace('\n', '').split(';')
+    map_object = map(int, element)
+    X = list(map_object)
+    a = int(lineass[i])
+    asdf = []
+    asdf.append(a)
+    array = []
+    array.append(X)
+    algoritmo.partial_fit(array, asdf, classes=np.unique([1,2,3,4,5]))
+    i = i + 1
+    print(i)
+print(algoritmo.class_count_)
+'''
+file_train_categories = open(filename_train_categories, 'r')
 lines = file_train_dataset.readlines()
+print(len(lines))
 lines_category = file_train_categories.readlines()
 for i in range(32529):
     print(i)
-    line = lines[i].replace('\n', '').split(';')
+    line = lines[i].split(',')
+    print(len(line))
     map_object = map(int, line)
     X = list(map_object)
     a = int(lines_category[i].replace('\n', ''))
@@ -28,3 +47,4 @@ for i in range(32529):
 
 print(algoritmo.class_count_)
 #https://stackoverflow.com/questions/42147302/sklearn-sgd-partial-fit
+'''
