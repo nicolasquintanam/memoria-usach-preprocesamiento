a8fe290f9dbd9c597512c4e679aba77b42606dfa
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-02-28 16:01:14 -0300

    prueba de entrenamiento

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -4,6 +4,7 @@ import multiprocessing
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
 import getopt
 import sys
 
@@ -23,8 +24,8 @@ for opt, arg in options:
         flujo = arg
 
 
-filename_train_dataset = flujo + '_dataset_train.csv'
-filename_train_categories = flujo + '_tenders-category-train.txt'
+filename_train_dataset = flujo + '/out_' + flujo + '_1_dataset_train.csv'
+filename_train_categories = flujo + '/out_' + flujo + '_1_categories_train.txt'
 
 algoritmo = GaussianNB()
 
@@ -44,25 +45,17 @@ for element in file_train_dataset:
     algoritmo.partial_fit(array, asdf, classes=np.unique([1,2,3,4,5]))
     i = i + 1
     print(i)
-print(algoritmo.class_count_)
-'''
-file_train_categories = open(filename_train_categories, 'r')
-lines = file_train_dataset.readlines()
-print(len(lines))
-lines_category = file_train_categories.readlines()
-for i in range(32529):
-    print(i)
-    line = lines[i].split(',')
-    print(len(line))
-    map_object = map(int, line)
-    X = list(map_object)
-    a = int(lines_category[i].replace('\n', ''))
-    asdf = []
-    asdf.append(a)
-    array = []
-    array.append(X)
-    algoritmo.partial_fit(array, asdf, classes=np.unique([1,2,3,4,5]))
 
+filename_test_categories = flujo + '/out_' + flujo + '_1_categories_validation.txt'
+file_test_categories = open(filename_test_categories, 'r')
+lineas_test = file_test_categories.readlines()
+y_test = []
+for elemento in lineas_test:
+    elemento = elemento.replace('\n', '')
+    y_test.append(y_test)
+
+print('la cantidad de líneas en y_test es = ' + str(len(y_test)))
+y_pred = algoritmo.predict(y_test)
+matriz = confusion_matrix(y_test, y_pred)
+print(matriz)
 print(algoritmo.class_count_)
-#https://stackoverflow.com/questions/42147302/sklearn-sgd-partial-fit
-'''
