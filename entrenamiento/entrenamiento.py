b7be6dc7921f0151a89a94d6c7342b1a0a75c33e
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-03-02 00:38:16 -0300

    ultima modificacion

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -9,7 +9,7 @@ from sklearn.metrics import accuracy_score
 import getopt
 import sys
 
-flujo = 'flujo_base'
+flujo = ''
 
 try:
     options, remainder = getopt.getopt(
@@ -87,107 +87,8 @@ for j in range(5):
     file_test_categories.close()
     file_train_dataset.close()
     file_train_categories.close()
-    print('finalizó iteración ' + str(i+1))
+    print('finalizó iteración ' + str(j+1) + ' del ' + flujo)
 
 accuracy_average = sum(list_accuracy)/len(list_accuracy)
 file_abstract.write('ACCURACY AVERAGE = ' + str(accuracy_average))
 file_abstract.close()
-
-#print(algoritmo.class_count_)
-'''
-from time import time
-import csv
-import multiprocessing
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_score
-import getopt
-import sys
-
-flujo = 'flujo_base'
-iteraciones = 1
-
-try:
-    options, remainder = getopt.getopt(
-        sys.argv[1:],
-        'i',
-        ['input='])
-except getopt.GetoptError as err:
-    print('ERROR:', err)
-    sys.exit(1)
-
-for opt, arg in options:
-    if opt in ('-i', '--input'):
-        flujo = arg
-
-filename_abstract = flujo + '/abstract/' + flujo + '.txt'
-file_abstract = open(filename_abstract, 'w')
-for i in range(iteraciones):
-    filename_train_dataset = flujo + '/out_' + flujo + '_' + str(i+1) + '_dataset_train.csv'
-    filename_train_categories = flujo + '/out_' + flujo + '_' + str(i+1) + '_categories_train.txt'
-
-    algoritmo = MultinomialNB()
-    file_train_dataset = open(filename_train_dataset, 'r')
-    file_train_categories = open(filename_train_categories, 'r')
-    lineass = file_train_categories.readlines()
-
-    j = 0
-    for element in file_train_dataset:
-        element = element.replace('\n', '').split(';')
-        map_object = map(int, element)
-        X = list(map_object)
-        a = int(lineass[i])
-        asdf = []
-        asdf.append(a)
-        array = []
-        array.append(X)
-        algoritmo.partial_fit(np.array(array), np.array(asdf), classes=np.unique([1,2,3,4,5]))
-        j = j + 1
-        print(j)
-
-    filename_test_dataset = flujo + '/out_' + flujo + '_' + str(i+1) + '_dataset_validation.csv'
-    filename_test_categories = flujo + '/out_' + flujo + '_' + str(i+1) + '_categories_validation.txt'
-    file_test_dataset = open(filename_test_dataset, 'r')
-    file_test_categories = open(filename_test_categories, 'r')
-    lineas_dataset_test = file_test_dataset.readlines()
-    lineas_categories_test = file_test_categories.readlines()
-    y_test = []
-    categories_rial = []
-    j = 0
-    for elemento in lineas_dataset_test:
-        elemento = elemento.replace('\n', '').split(';')
-        map_object = map(int, elemento)
-        X = list(map_object)
-        categories_rial.append(int(lineas_categories_test[j]))
-        j = j + 1
-        y_test.append(X)
-
-    y_pred = algoritmo.predict(np.array(y_test))
-    exactitud = accuracy_score(categories_rial, y_pred)
-    #precision = precision_score(categories_rial, y_pred)
-    #sensibilidad = recall_score(categories_rial, y_pred)
-    #f1score = f1_score(categories_rial, y_pred)
-    matriz = confusion_matrix(categories_rial, y_pred)
-    file_abstract.write('Iteración ' + str(i+1) + ': \n\n')
-    file_abstract.write('Accuracy = ' + str(exactitud) + '\n')
-    #file_abstract.write('Precision = ' + str(precision) + '\n')
-    #file_abstract.write('Recall = ' + str(sensibilidad) + '\n')
-    #file_abstract.write('F1 Score = ' + str(f1score) + '\n')
-    file_abstract.write('Confusion Matrix \n\n')
-    print(matriz)
-    file_abstract.write(np.array2string(matriz, separator=', '))
-
-    file_abstract.write('\n\n\n')
-
-    file_train_dataset.close()
-    file_train_categories.close()
-    file_test_dataset.close()
-    file_test_categories.close()
-    print('finalicé entrenamiento iteración ' + str(i+1))
-file_abstract.close()
-    '''
