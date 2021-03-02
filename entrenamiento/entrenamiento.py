fa790d1e571d3b986dad1877acdcf23c3943868b
Nicolás Quintana <nicolas.quintana@usach.cl>
2021-03-01 22:15:03 -0300

    prueba

--- entrenamiento/entrenamiento.py
+++ entrenamiento/entrenamiento.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 import getopt
 import sys
 
@@ -24,60 +25,171 @@ for opt, arg in options:
         flujo = arg
 
 
-filename_train_dataset = flujo + '/out_' + flujo + '_2_dataset_train.csv'
-filename_train_categories = flujo + '/out_' + flujo + '_2_categories_train.txt'
+filename_abstract = 'abstract/' + flujo + '.txt'
+file_abstract = open(filename_abstract, 'w')
+for j in range(5):
 
-algoritmo = MultinomialNB()
+    filename_train_dataset = flujo + '/out_' + flujo + '_'+str(j+1)+'_dataset_train.csv'
+    filename_train_categories = flujo + '/out_' + flujo + '_'+str(j+1)+'_categories_train.txt'
 
-file_train_dataset = open(filename_train_dataset, 'r')
-file_train_categories = open(filename_train_categories, 'r')
-lineass = file_train_categories.readlines()
-i = 0
+    algoritmo = MultinomialNB()
 
-xx = []
-yy = []
-for element in file_train_dataset:
-    element = element.replace('\n', '').split(';')
-    map_object = map(int, element)
-    X = list(map_object)
-    a = int(lineass[i])
-    asdf = []
-    asdf.append(a)
-    array = []
-    array.append(X)
-    #xx.append(np.array(X))
-    #yy.append(a)
-    #print(np.array(asdf))
-    #print(np.array(array))
-    #print(np.unique([1,2,3,4,5]))
-    print(i)
-    algoritmo.partial_fit(np.array(array), np.array(asdf), classes=np.unique([1,2,3,4,5]))
-    i = i + 1
+    file_train_dataset = open(filename_train_dataset, 'r')
+    file_train_categories = open(filename_train_categories, 'r')
+    lineass = file_train_categories.readlines()
+    i = 0
 
-#algoritmo.fit(xx, yy)
+    for element in file_train_dataset:
+        element = element.replace('\n', '').split(';')
+        map_object = map(int, element)
+        X = list(map_object)
+        a = int(lineass[i])
+        asdf = []
+        asdf.append(a)
+        array = []
+        array.append(X)
+        print(i)
+        algoritmo.partial_fit(np.array(array), np.array(asdf), classes=np.unique([1,2,3,4,5]))
+        i = i + 1
 
-filename_test_dataset = flujo + '/out_' + flujo + '_2_dataset_validation.csv'
-filename_test_categories = flujo + '/out_' + flujo + '_2_categories_validation.txt'
-file_test_dataset = open(filename_test_dataset, 'r')
-file_test_categories = open(filename_test_categories, 'r')
-lineas_dataset_test = file_test_dataset.readlines()
-lineas_categories_test = file_test_categories.readlines()
-y_test = []
-categories_rial = []
-i = 0
-for elemento in lineas_dataset_test:
-    elemento = elemento.replace('\n', '').split(';')
-    map_object = map(int, elemento)
-    X = list(map_object)
-    categories_rial.append(int(lineas_categories_test[i]))
-    i = i + 1
-    y_test.append(X)
+    #algoritmo.fit(xx, yy)
+
+    filename_test_dataset = flujo + '/out_' + flujo + '_'+str(j+1)+'_dataset_validation.csv'
+    filename_test_categories = flujo + '/out_' + flujo + '_'+str(j+1)+'_categories_validation.txt'
+    file_test_dataset = open(filename_test_dataset, 'r')
+    file_test_categories = open(filename_test_categories, 'r')
+    lineas_dataset_test = file_test_dataset.readlines()
+    lineas_categories_test = file_test_categories.readlines()
+    y_test = []
+    categories_rial = []
+    i = 0
+    for elemento in lineas_dataset_test:
+        elemento = elemento.replace('\n', '').split(';')
+        map_object = map(int, elemento)
+        X = list(map_object)
+        categories_rial.append(int(lineas_categories_test[i]))
+        i = i + 1
+        y_test.append(X)
 
 
-print('la cantidad de líneas en y_test es = ' + str(len(y_test)))
-y_pred = algoritmo.predict(np.array(y_test))
-print(y_pred)
-print(categories_rial)
-matriz = confusion_matrix(categories_rial, y_pred)
-print(matriz)
+    print('la cantidad de líneas en y_test es = ' + str(len(y_test)))
+    y_pred = algoritmo.predict(np.array(y_test))
+    print(y_pred)
+    print(categories_rial)
+    matriz = confusion_matrix(categories_rial, y_pred)
+    print(matriz)
+    exactitud = accuracy_score(categories_rial, y_pred)
+    print(exactitud)
+
+    exactitud = accuracy_score(categories_rial, y_pred)
+    matriz = confusion_matrix(categories_rial, y_pred)
+    file_abstract.write('Iteración ' + str(i+1) + ': \n\n')
+    file_abstract.write('Accuracy = ' + str(exactitud) + '\n')
+    file_abstract.write('Confusion Matrix \n\n')
+    print(matriz)
+    file_abstract.write(np.array2string(matriz, separator=', '))
+    file_abstract.write('\n\n\n')
+
+    file_test_dataset.close()
+    file_test_categories.close()
+    file_train_dataset.close()
+    file_train_categories.close()
+file_abstract.close()
 #print(algoritmo.class_count_)
+'''
+from time import time
+import csv
+import multiprocessing
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+import getopt
+import sys
+
+flujo = 'flujo_base'
+iteraciones = 1
+
+try:
+    options, remainder = getopt.getopt(
+        sys.argv[1:],
+        'i',
+        ['input='])
+except getopt.GetoptError as err:
+    print('ERROR:', err)
+    sys.exit(1)
+
+for opt, arg in options:
+    if opt in ('-i', '--input'):
+        flujo = arg
+
+filename_abstract = flujo + '/abstract/' + flujo + '.txt'
+file_abstract = open(filename_abstract, 'w')
+for i in range(iteraciones):
+    filename_train_dataset = flujo + '/out_' + flujo + '_' + str(i+1) + '_dataset_train.csv'
+    filename_train_categories = flujo + '/out_' + flujo + '_' + str(i+1) + '_categories_train.txt'
+
+    algoritmo = MultinomialNB()
+    file_train_dataset = open(filename_train_dataset, 'r')
+    file_train_categories = open(filename_train_categories, 'r')
+    lineass = file_train_categories.readlines()
+
+    j = 0
+    for element in file_train_dataset:
+        element = element.replace('\n', '').split(';')
+        map_object = map(int, element)
+        X = list(map_object)
+        a = int(lineass[i])
+        asdf = []
+        asdf.append(a)
+        array = []
+        array.append(X)
+        algoritmo.partial_fit(np.array(array), np.array(asdf), classes=np.unique([1,2,3,4,5]))
+        j = j + 1
+        print(j)
+
+    filename_test_dataset = flujo + '/out_' + flujo + '_' + str(i+1) + '_dataset_validation.csv'
+    filename_test_categories = flujo + '/out_' + flujo + '_' + str(i+1) + '_categories_validation.txt'
+    file_test_dataset = open(filename_test_dataset, 'r')
+    file_test_categories = open(filename_test_categories, 'r')
+    lineas_dataset_test = file_test_dataset.readlines()
+    lineas_categories_test = file_test_categories.readlines()
+    y_test = []
+    categories_rial = []
+    j = 0
+    for elemento in lineas_dataset_test:
+        elemento = elemento.replace('\n', '').split(';')
+        map_object = map(int, elemento)
+        X = list(map_object)
+        categories_rial.append(int(lineas_categories_test[j]))
+        j = j + 1
+        y_test.append(X)
+
+    y_pred = algoritmo.predict(np.array(y_test))
+    exactitud = accuracy_score(categories_rial, y_pred)
+    #precision = precision_score(categories_rial, y_pred)
+    #sensibilidad = recall_score(categories_rial, y_pred)
+    #f1score = f1_score(categories_rial, y_pred)
+    matriz = confusion_matrix(categories_rial, y_pred)
+    file_abstract.write('Iteración ' + str(i+1) + ': \n\n')
+    file_abstract.write('Accuracy = ' + str(exactitud) + '\n')
+    #file_abstract.write('Precision = ' + str(precision) + '\n')
+    #file_abstract.write('Recall = ' + str(sensibilidad) + '\n')
+    #file_abstract.write('F1 Score = ' + str(f1score) + '\n')
+    file_abstract.write('Confusion Matrix \n\n')
+    print(matriz)
+    file_abstract.write(np.array2string(matriz, separator=', '))
+
+    file_abstract.write('\n\n\n')
+
+    file_train_dataset.close()
+    file_train_categories.close()
+    file_test_dataset.close()
+    file_test_categories.close()
+    print('finalicé entrenamiento iteración ' + str(i+1))
+file_abstract.close()
+    '''
